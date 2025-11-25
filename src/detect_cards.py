fda4ffc4b6a0a22d975432f2298d3ed1bbdf76e6
KAMISENHEIMER <therealspydude@gmail.com>
2025-11-24 19:34:51 -0600

    moved testing into ints own file

--- src/detect_cards.py
+++ src/detect_cards.py
@@ -65,25 +65,3 @@ def detect_rectangles(image_path):
             rectangles.append(corners)
 
     return rectangles
-
-
-if __name__ == "__main__":
-    import sys
-
-    #example images
-    # image_path = "test_images/5cardsbasic.png"
-    image_path = "test_images/5cardswithclutter.png"
-    # image_path = "test_images/3cardswithclutter.png" #failure case, chip on card
-    # image_path = "test_images/5cardswithhands.png" #failure case, stylized background
-    # image_path = "test_images/boonecards.png"
-    
-    #detect rectangles in image
-    rects = detect_rectangles(image_path)
-
-    # prints out detected rectangles
-    # print("Rectangles detected:", len(rects))
-    # for i, rect in enumerate(rects):
-    #     print(f"Rectangle {i+1} corners:", rect)
-
-    #draw detected rectangles on image
-    draw_rectangles(image_path, rects)
